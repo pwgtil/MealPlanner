db0c1227acb0674d8b1ddf1a216cc749e6f1d7bf
pwgtiluser.email <87475830+pwgtil@users.noreply.github.com>
2023-05-29 16:33:20 +0200

    Meal Planner: Stage 3/6: Database storage v3.0

--- task/src/mealplanner/Main.kt
+++ task/src/mealplanner/Main.kt
@@ -1,4 +1,6 @@
 package mealplanner
+import java.sql.Connection
+import java.sql.DriverManager
 
 const val TXT_CHOSE_OPERATION = "What would you like to do (add, show, exit)?"
 const val TXT_CHOOSE_CATEGORY = "Which meal do you want to add (breakfast, lunch, dinner)?"
@@ -16,29 +18,72 @@ val VALID_OPS = listOf("add", "show", "exit")
 val VALID_CATEGORIES = listOf("breakfast", "lunch", "dinner")
 
 fun main() {
-    val mealsDB = mutableListOf<Meal>()
+    val connection = createDB()
     while (true) {
         TXT_CHOSE_OPERATION.let(::println)
         val operation = readln()
         if (operation !in VALID_OPS) continue
         when (operation) {
             VALID_OPS[0] -> {
-                mealsDB.add(addMeal())
+                val meal = addMeal()
+                addMealToDB(meal, connection)
             }
 
             VALID_OPS[1] -> {
-                showMeals(mealsDB)
+                val meals = getMealsFromDV(connection)
+                showMeals(meals)
             }
 
             VALID_OPS[2] -> {
                 TXT_BYE.let(::println)
+                connection.close()
                 break
             }
         }
     }
 }
 
-fun showMeals(meals: MutableList<Meal>) {
+fun createDB(): Connection {
+    val connection = DriverManager.getConnection("jdbc:sqlite:meals.db")
+    val statement = connection.createStatement()
+//    statement.executeUpdate("drop table if exists meals")
+//    statement.executeUpdate("drop table if exists ingredients")
+//    statement.executeUpdate("create table meals (category string, meal string, meal_id integer)")
+//    statement.executeUpdate("create table ingredients (ingredient string, ingredient_id integer, meal_id integer)")
+    statement.executeUpdate("create table if not exists meals (category string, meal string, meal_id integer)")
+    statement.executeUpdate("create table if not exists ingredients (ingredient string, ingredient_id integer, meal_id integer)")
+    return connection
+}
+
+fun addMealToDB(meal: Meal, connection: Connection) {
+    val statement = connection.createStatement()
+    val lastID = statement.executeQuery("select max(meal_id) from meals").getInt("max(meal_id)")
+    statement.executeUpdate("insert into meals values('${meal.category}', '${meal.name}', ${lastID + 1})")
+    meal.ingredients.forEachIndexed { i, s ->
+        statement.executeUpdate("insert into ingredients values('${s}', '$i', ${lastID + 1})")
+    }
+}
+
+fun getMealsFromDV(connection: Connection): List<Meal> {
+    val statement = connection.createStatement()
+    val mealsRS = statement.executeQuery("select * from meals")
+    val mealsStructured = mutableMapOf<Int, Meal>()
+    while (mealsRS.next()){
+        val id = mealsRS.getInt("meal_id")
+        val category = mealsRS.getString("category")
+        val name = mealsRS.getString("meal")
+        val ingStatement = connection.createStatement()
+        val ingredientsRS = ingStatement.executeQuery("select * from ingredients where meal_id = '$id'")
+        val ingredients = mutableListOf<String>()
+        while (ingredientsRS.next()) {
+            ingredients.add(ingredientsRS.getString("ingredient"))
+        }
+        mealsStructured[id] = Meal(category, name, ingredients)
+    }
+    return mealsStructured.values.toList()
+}
+
+fun showMeals(meals: List<Meal>) {
     if (meals.size == 0) {
         TXT_NO_MEALS.let(::println)
     } else {
